bb6fd875317d0d1374c617698d81bdacbd068c87
Julen García Gonzalo <jgarciago.inf@upsa.es>
2024-10-21 13:43:09 +0200

    Modulo de la página principal creado
    -Falta darle estilo
    -Falta escribir bien la ruta de las imagenes usadas
    - A lo mejor habría que cambiar el símbolo + por una flecha circular

---
+++ front/src/app/pag-inicio/pag-inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-inicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './pag-inicio.component.html',
+  styleUrl: './pag-inicio.component.css'
+})
+export class PagInicioComponent {
+
+}
