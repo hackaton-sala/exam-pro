036d5bc297b7d89219889c717ae43c7fa4748044
Jaime <jaimegonzalezfernandez10@gmail.com>
2024-10-22 16:31:27 +0200

    Header y footer en todas las páginas

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -14,13 +14,17 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     NoPageFoundComponent,
     AppComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     AppRoutingModule,
