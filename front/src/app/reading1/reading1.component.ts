60ddcfa651a077ab0ff37ebdeab1915707bcb234
Julen García Gonzalo <jgarciago.inf@upsa.es>
2024-10-22 12:05:48 +0200

    Reading1
    Generador del reading

---
+++ front/src/app/reading1/reading1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reading1',
+  standalone: true,
+  imports: [],
+  templateUrl: './reading1.component.html',
+  styleUrl: './reading1.component.css'
+})
+export class Reading1Component {
+
+}
