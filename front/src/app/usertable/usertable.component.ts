8f583273503775c63898007c6f296810af4591ef
Fernando Baliña <fbalinano.inf@upsa.es>
2024-10-22 10:24:14 +0200

    Tabla
    añado usertable con su html y css

---
+++ front/src/app/usertable/usertable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usertable',
+  standalone: true,
+  imports: [],
+  templateUrl: './usertable.component.html',
+  styleUrl: './usertable.component.css'
+})
+export class UsertableComponent {
+
+}
