b46311beb9b33cee3fae580ba7bec376547948eb
Lander Perez <landerperez.maat@gmail.com>
2024-10-23 10:09:09 +0200

    llamar del front al back de py

--- front/src/app/reading1/reading1.component.ts
+++ front/src/app/reading1/reading1.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GrammarService } from '../services/application/gramar.service';
 
 @Component({
   selector: 'app-reading1',
   templateUrl: './reading1.component.html',
   styleUrl: './reading1.component.css'
 })
-export class Reading1Component {
+export class Reading1Component implements OnInit {
+  
+  constructor(public grammarService: GrammarService) {
 
+  }
+  
+  ngOnInit(): void {
+    this.generateQuestions();
+  }
+
+  generateQuestions() {
+    const prompt = "Generate 5 grammar questions for a B2 level English exam with 4 options each.";
+    this.grammarService.generateQuestions(prompt).subscribe(
+      (response) => {
+        console.log(response);  // Mostrar las preguntas generadas
+      },
+      (error) => {
+        console.error('Error al generar preguntas', error);
+      }
+    );
+  }
 }
---
+++ front/src/app/services/application/gramar.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { User } from '../../models/application/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GrammarService {
+
+  private url = `${environment.urlPy}/${'generate-questions'}`;
+
+  constructor(private httpClient: HttpClient) { }
+
+  generateQuestions(prompt: string): Observable<any> {
+    const body = { prompt: prompt };
+    return this.httpClient.post<any>(this.url, body);
+  }
+}
