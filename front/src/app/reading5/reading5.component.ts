34c3e0148e4ac62222555235031382d2dae39420
Julen García Gonzalo <jgarciago.inf@upsa.es>
2024-10-23 11:09:55 +0200

    Cambios en el reading 5
    Ahora el reading 5 genera las opciones de forma dinámica

--- front/src/app/reading5/reading5.component.ts
+++ front/src/app/reading5/reading5.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-reading5',
   templateUrl: './reading5.component.html',
-  styleUrl: './reading5.component.css'
+  styleUrls: ['./reading5.component.css']
 })
 export class Reading5Component {
+  // Usar un tipo de objeto con firma de índice
+  selectedOptions: { [key: string]: string } = {
+    opcionA: '',
+    opcionB: '',
+    opcionC: '',
+    opcionD: '',
+    opcionE: '',
+    opcionF: ''
+  };
 
+  // Método para habilitar el botón de enviar
+  isSubmitDisabled(): boolean {
+    return !Object.values(this.selectedOptions).every(option => option !== '');
+  }
+
+  // Método para manejar el envío de respuestas
+  submitResponses() {
+    console.log('Respuestas enviadas:', this.selectedOptions);
+    // Aquí puedes agregar la lógica para manejar el envío
+  }
 }
